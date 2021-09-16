09d1d85614a890f2dfaacbc8e00b0d7b4c46ec48
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-09-16 16:07:00 -0700

    first commit

---
+++ ts_tutorial_with_theNetNinja/sandbox.js
@@ -0,0 +1,11 @@
+var character = "mario";
+// console.log(character);
+// const inputs = document.querySelectorAll("input");
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+var circumfenrence = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circumfenrence(20));
+// how to compile... tsc filename.ts -w
---
+++ ts_tutorial_with_theNetNinja/sandbox.ts
@@ -0,0 +1,16 @@
+const character = "mario";
+
+// console.log(character);
+
+// const inputs = document.querySelectorAll("input");
+
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+const circumfenrence = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circumfenrence(20));
+
+// how to compile... tsc filename.ts -w
