2e577d99ebd516efeec20524d22765fd45a5955f
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-10-12 01:35:23 -0700

    add more classes

--- ts_tutorial_with_theNetNinja/public/app.js
+++ ts_tutorial_with_theNetNinja/public/app.js
@@ -22,6 +22,7 @@
 // console.log(me);
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 // ! .js, not .ts!
 // let docOne: HasFormatter;
 // let docTwo: HasFormatter;
@@ -57,6 +58,9 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -66,5 +70,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
--- ts_tutorial_with_theNetNinja/src/app.ts
+++ ts_tutorial_with_theNetNinja/src/app.ts
@@ -27,6 +27,7 @@
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 // ! .js, not .ts!
 
 // let docOne: HasFormatter;
@@ -75,6 +76,10 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   let doc: HasFormatter;
@@ -83,5 +88,5 @@ form.addEventListener("submit", (e: Event) => {
   } else {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-  console.log(doc);
+  list.render(doc, type.value, "end");
 });
